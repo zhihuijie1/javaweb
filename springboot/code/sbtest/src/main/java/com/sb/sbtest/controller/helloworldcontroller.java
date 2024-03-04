22ce6d15361b94cfbdfa2b26e5621e01f35cc5e0
yibai <404223879@qq.com>
2024-03-04 21:45:58 +0800

    使用value 与 environment 注解来提取配置文件中的信息

--- springboot/code/sbtest/src/main/java/com/sb/sbtest/controller/helloworldcontroller.java
+++ springboot/code/sbtest/src/main/java/com/sb/sbtest/controller/helloworldcontroller.java
@@ -1,12 +1,30 @@
 package com.sb.sbtest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class helloworldcontroller {
+    /** 使用value注解提取配置文件中的信息 **/
+    @Value("${person.name2}")
+    private String girlfriend;
+    @Value("${city[0]}")
+    private String liveplace;
+    @Value("${zoo.zooname}")
+    private String zooplace;
+
+    /** 使用environment注解提取配置文件中的信息**/
+    @Autowired
+    Environment env;
+
     @RequestMapping ("/hello")
     public String hello() {
-        return "hello world";
+
+        String str = girlfriend + " " + liveplace + " " + zooplace;
+        String str2 = env.getProperty("person.name1") + " " + env.getProperty("person.age1");
+        return str2;
     }
 }
